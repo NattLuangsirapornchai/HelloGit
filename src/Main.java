92cdee15f5d465ac09a9b88960da4c84d3cb0f48
Natt Luangsirapornchai <tantawan166@gmail.com>
2017-08-25 15:34:57 +0700

    Add function to calculate the area of rectangle

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Hello Git");
-
+		System.out.println("I am Natt");
+		System.out.println("Enter rectangle width :");
+		int a  = sc.nextInt();
+		System.out.println("Enter rectangle height :");
+		int b  = sc.nextInt();
+		System.out.println("The rectangle area is "+(a*b));
 	}
 
 }
